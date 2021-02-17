ccda2d231dd86ac74b798a575d6d88465d0efe0a
Lucas Leão <ilolt.llol@gmail.com>
2021-02-16 22:31:08 -0400

    Update estrutura.py

--- e-mail/estrutura.py
+++ e-mail/estrutura.py
@@ -1,6 +1,5 @@
 mensagem = f"""Relatório Interativo. \nPreferível pelo PC. Caso queira pelo celular, selecione no navegador para visualizar a versão para computador.
 
-[MUDANÇA]: Agora, estarei upando diretamente para um site meu, sem necessidade de baixar
 Link: https://heylucasleao.github.io/
 
 \nPara entrar em contato comigo: https://t.me/heylucasleao
