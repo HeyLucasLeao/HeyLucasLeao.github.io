3153e7aae1085007bcda2d1bed573642d8a082c2
Lucas Leão <ilolt.llol@gmail.com>
2021-02-17 21:20:25 -0400

    Delete tempCodeRunnerFile.py

--- e-mail/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-data = str(dt.datetime.now())[:10]
-df_estado = pd.read_csv(
-    'https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
-df_estado_soma_casos = df_estado.query(
-    f"state == 'AM' and date == '{data}'")['newCases'][1]
